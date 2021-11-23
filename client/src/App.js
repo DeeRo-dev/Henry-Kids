45001889ab14b89a2a006a56baa5c2efeb3a3132
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-11-23 12:26:38 -0500

    proof message in app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    /* Add Browser Route */
     <BrowserRouter>
       <div className="App">
         <Routes></Routes>
