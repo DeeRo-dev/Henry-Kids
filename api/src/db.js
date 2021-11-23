80ad4998a80556de0f638d046194a738f0d48304
felipe sanchez <felipesanchez_13@hotmail.com>
2021-11-23 18:46:02 -0300

    se crea la realcion mucho a mucho Calse con Category

--- api/src/db.js
+++ api/src/db.js
@@ -32,11 +32,11 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { clases , category} = sequelize.models;
+const { Clase , Category} = sequelize.models;
 
 // Aca vendrian las relaciones
-// Curso.belongsToMany(Categori,{through:'tipoCurso'})
-// Categori.belongsToMany(Curso,{through:'TipoCurso'})
+Clase.belongsToMany(Category,{through:'tipoCurso'})
+Category.belongsToMany(Clase,{through:'TipoCurso'})
 // Categori.hasMany(Curso);
 
 
