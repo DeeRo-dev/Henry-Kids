4c1d306387c03f497fec5a1c181ebaea29da080a
felipe sanchez <felipesanchez_13@hotmail.com>
2021-11-23 23:20:08 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,4 @@
 const { Router } = require("express");
-const demoRutes = require("./demo");
 const categoryRutes = require("./category");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -9,7 +8,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/category", categoryRutes); // esto me genera /kids/category*  (traigo las rotuterdel archivo pokemon.js)
-router.use("/demo", demoRutes); 
+ 
 
 module.exports = router;
 
