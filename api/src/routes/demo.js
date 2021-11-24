661e63ecf3a737a8682a00dbe298f9702bd145c7
felipe sanchez <felipesanchez_13@hotmail.com>
2021-11-23 23:18:16 -0300

    Delete demo.js

--- api/src/routes/demo.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { Curso ,Categori } = require("../db")
-
-const router = Router();
-
-router.get("/", async (req, res, next) => {})
-
-module.exports = router;
