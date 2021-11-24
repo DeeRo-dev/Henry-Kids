d527db622258af642da3ff416d85243e1831444f
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-11-23 22:46:41 -0500

    LandingPage Finish

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,34 @@
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.css";
-import { Button } from '@material-ui/core';
+import { Button } from "@material-ui/core";
 
-export default function LandingPage(){
-    return(
-        <div className={styles.containerBackground}>
-            <div className={styles.background}>
-            <Button className={styles.btn} variant="contained" color="primary">Hello World</Button>
-            {/* <Link to="/home">Home</Link>  */}  
-            </div>
-        
-        </div>
-    )
+export default function LandingPage() {
+  return (
+    <div className={styles.containerBackground}>
+
+      <div className={styles.background}>
+      <img
+          className={styles.logo}
+          src="https://i.imgur.com/AWEe2XR.png"
+          alt="img"
+        ></img>
+        <Button
+          className={styles.buttonIngresar}
+          variant="contained"
+          color="primary"
+        >
+          Ingresar
+        </Button>
+        <Button
+          className={styles.buttonRegistrarse}
+          variant="contained"
+          color="primary"
+        >
+          Registrarse
+        </Button>
+
+        {/* <Link to="/home">Home</Link>  */}
+      </div>
+    </div>
+  );
 }
