7a82b060ee3692234abd9b067a462ccd1495f03c
Gaston Ripamonti <gaston081@hotmail.com>
2021-11-25 15:49:22 -0300

    terminado parcial card

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.jsx"
 import Nav from "./components/Nav.jsx"
+import Card from "./components/Card/Card";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
         <Route path="/" element = {<LandingPage/>}/>
         <Route path="/home" element = {<Nav/>}/>
+        <Route path="/card" element = {<Card/>}/>
         </Routes>
       </div>
     </BrowserRouter>
