1941b2d4e60727a8549b32b782d1b23113c43306
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2021-11-25 16:41:01 -0300

    se creo un boton para crear clases

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage.jsx"
-
+import FormularioClase from "./components/FormularioClase/FormularioClase.jsx";
 import Home from "./components/Home/home";
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
         <Routes>
         <Route path="/" element = {<LandingPage/>}/>
         <Route path="/home" element = {<Home/>}/>
+        <Route path="/create-clase" element = {<FormularioClase/>}/>
         </Routes>
       </div>
     </BrowserRouter>
--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -6,16 +6,49 @@ import { Link } from "react-router-dom";
 
 
 export default function Home(){
+  
   return (
   <div >
     <div>
          <Nav/>
     </div>
- 
+ <div>
+    <Link to="/create-clase">
+        <button>
+      Crear una clase
+   </button>
+      </Link>
+  
+ </div>
 <div className="contentCards">
-    <div>
-      
-    </div>
+
+{/* //base de datos 
+//que tenga info harcord
+//que sea un arrray de 10 elementos con la info que necesitamos */}
+
+
+{/*     
+      {
+        LOQUETENGODEBASEDEDATOS?.map(e => {
+         
+           return (
+            <div className='cartas'  key={e.id}>
+             
+              <Card id={e.id}              
+              title={e.title} 
+              category={e.category}
+              description={e.description} 
+              video_link={e.video_link} 
+              difficulty={e.difficulty}
+              game_link={e.game_link} 
+              valoration={e.valoration}
+              />
+              
+            </div>
+          
+            )})
+      } */}
+  
 </div>
 
   </div>
