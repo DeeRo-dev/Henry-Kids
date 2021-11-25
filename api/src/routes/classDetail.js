79af42311cf57c97a2ece637f6d138fcadcc9852
Ignacio Páez <ignaciopaez96@gmail.com>
2021-11-25 14:58:01 -0300

    commit 1 - prueba ruta detalle
    commit 1 a rama Crear-Ruta-Detalle-ID
    
    Prubea 1.

---
+++ api/src/routes/classDetail.js
@@ -0,0 +1,19 @@
+const { Class, Category, Evaluation } = require("../db");
+const router = require("./allClasses");
+
+// Ruta para traer detalle por id - Incluye modelo Class, Category y Evaluation
+
+router.get("/:id/", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const classDetail = await Class.findAll({
+      where: {
+        id: id,
+      },
+      include: { model: Category, Evaluation },
+    });
+    res.send(classDetail);
+  } catch (error) {
+    next(error);
+  }
+});
