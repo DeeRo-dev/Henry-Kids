26fdc8689f634f19170307eee4044c2c4c43ad8d
felipe sanchez <felipesanchez_13@hotmail.com>
2021-11-25 23:47:23 -0300

    Se Crea el User GetByID
    http://localhost:3001/user/:Id

--- api/src/models/Class.js
+++ api/src/models/Class.js
@@ -1,62 +1,65 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (Sequelize) => {
-  return Sequelize.define(
-    "class",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        primaryKey: true,
+  return Sequelize.define("class", {
+    // id: {
+    //   type: DataTypes.UUID,
+    //   defaultValue: DataTypes.UUIDV4,
+    //   allowNull: false,
+    //   primaryKey: true,
+    // },
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
       },
-      title: {
-        type: DataTypes.STRING,
-        validate: {
-          notEmpty: true,
-        },
-        allowNull: false,
-        unique: true,
+      allowNull: false,
+      unique: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      validate: {
+        notEmpty: true,
       },
-      description: {
-        type: DataTypes.TEXT,
-        validate: {
-          notEmpty: true,
-        },
-        allowNull: false,
+      allowNull: false,
+    },
+    studio_material: {
+      type: DataTypes.STRING(300),
+      validate: {
+        isUrl: true,
       },
-      studio_material: {
-        type: DataTypes.STRING(300),
-        validate: {
-          isUrl: true,
-        },
+    },
+    video_link: {
+      type: DataTypes.STRING(300),
+      validate: {
+        isUrl: true,
       },
-      video_link: {
-        type: DataTypes.STRING(300),
-        validate: {
-          isUrl: true,
-        },
+    },
+    game_link: {
+      type: DataTypes.STRING(300),
+      validate: {
+        isUrl: true,
       },
-      game_link: {
-        type: DataTypes.STRING(300),
-        validate: {
-          isUrl: true,
-        },
-      },
-      state: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-      },
-      difficulty: {
-        type: DataTypes.ENUM("Basica", "Intermedia", "Alta"),
-        allowNull: false,
-      },
-      // date: {
-      //   type: DataTypes.DATE,
-      //   // allowNull: true,
-      // },
-    }
-  );
+    },
+    state: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    difficulty: {
+      type: DataTypes.ENUM("Basica", "Intermedia", "Alta"),
+      allowNull: false,
+    },
+    // date: {
+    //   type: DataTypes.DATE,
+    //   // allowNull: true,
+    // },
+  });
 };
 
 // ID ✅
