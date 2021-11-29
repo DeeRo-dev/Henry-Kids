96b550f152da6f6ff81b9c02e38888f1b2115272
Yanina-brancolino <yanina.brancolino.93@gmail.com>
2021-11-29 18:10:20 -0300

    cambios

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,17 +1,12 @@
 const { Router } = require('express');
-const { createUser,getUserId, deleteUser, editUser} = require('../controllers/user');
-const { Router } = require("express");
-const { createUser, getUserId, getUser } = require("../controllers/user");
+const { createUser,getUserId, deleteUser, editUser, getUser} = require('../controllers/user');
 
 const router = Router();
-
-router.post("/", createUser);
-router.get("/:id", getUserId);
-router.get("/", getUser);
 
 router.post('/', createUser);
 router.get('/:id', getUserId);
 router.delete('/:id', deleteUser);
 router.put('/:id', editUser);
+router.get("/", getUser);
 
 module.exports = router;
