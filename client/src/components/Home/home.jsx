420ac851b1a53c7822ee5817de59942b331b3bc4
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-11-30 16:13:44 -0500

    avance protección de rutas

--- client/src/components/Home/home.jsx
+++ client/src/components/Home/home.jsx
@@ -1,17 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Nav from "../Nav/Nav.jsx";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { styled } from "@material-ui/core";
 import styles from "./Home.module.css";
 import Card from "../Card/Card.jsx";
 import { getAllclasses } from "../../actions/index.js";
+import { auth } from "../../firebase/firebaseConfig";
 
 export default function Home() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const allClasses = useSelector((state) => state.allClasses);
 
   useEffect(() => dispatch(getAllclasses()), [dispatch]);
+
+  function signOutUser(e) {
+    auth
+      .signOut(auth)
+      .then(() => {
+        console.log("done");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <div className={styles.home}>
@@ -20,18 +34,28 @@ export default function Home() {
       </div>
 
       <div className={styles.cards}>
-      {allClasses.map((e) => { 
-        return (<div key= {e.id}> <Link to={'/home/'+ e.id}> <Card 
-        id={e.id}
-        title={e.title}
-        category={e.category}
-        description={e.description}
-        video_link={e.video_link}
-        difficulty={e.difficulty}
-        game_link={e.game_link}
-        valoration={e.valoration}
-        /> </Link></div>)}
-      )} 
+        <button onClick={signOutUser}> Salir </button>
+
+        {allClasses.map((e) => {
+          return (
+            <div key={e.id}>
+              {" "}
+              <Link to={"/home/" + e.id}>
+                {" "}
+                <Card
+                  id={e.id}
+                  title={e.title}
+                  category={e.category}
+                  description={e.description}
+                  video_link={e.video_link}
+                  difficulty={e.difficulty}
+                  game_link={e.game_link}
+                  valoration={e.valoration}
+                />{" "}
+              </Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- client/src/components/PrivateRoute/PrivateRoute.jsx
+++ client/src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,53 +1,15 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { AuthContext } from '../context/auth';
+import { Route, Navigate } from "react-router-dom";
+import { auth } from "../../firebase/firebaseConfig";
 
-class PrivateRoute extends React.Component {
-    render() {
-        const {
-            type,
-            history,
-            ...rest
-        } = this.props;
-        const {
-            isLoggedIn,
-        } = this.context;
 
-        if (type === 'private' && !isLoggedIn) {
-            return <Redirect to="/" />;
-        } else if (type === 'public' && isLoggedIn) {
-            return <Redirect to="/home" />;
-        }
-
-        return <Route {...rest} />;
-    };
+export default function PrivateRoute({component: Component, ...rest}) {
+    auth.onAuthStateChanged((user) => {
+        console.log(user)
+    });
+    return(
+        <Route {...rest}> {auth ? <Component/> : <Navigate to={'/'}/> }</Route>
+    )
 }
 
-PrivateRoute.contextType = AuthContext;
-
-export default PrivateRoute;
 
 
-// if (isAuthenticated) {
-//     return <Route path="/home/student" element = {<Home/>}/>
-//     // <Route path="/home/teacher" element = {<HomeTeacher/>}/>
-//     // <Route path="/create-clase" element = {<FormularioClase/>}/>
-//     // <Route path="/home/:id" element = {<ClassDetail/>}/> 
-    
-//       } else {
-//           return <Redirect to="/" />
-//         }
-//     }
-
-
-    
-       
-
-
-
-{/* <Route path="/" element={<LandingPage />} />
-          <PrivateRoute type="public" path="/" component={LandingPage} />
-          <Route path="/home/student" element={<Home />} />
-          <Route path="/home/teacher" element={<HomeTeacher />} />
-          <Route path="/create-clase" element={<FormularioClase />} />
-          <Route path="/home/:id" element={<ClassDetail />} /> */}
