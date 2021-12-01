f6a3fdb6c1ad5a087e20fd9df74284d4ac0293b5
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-11-30 22:25:40 -0500

    Co-authored-by: giovannacilfone2@gmail.com <giovannacilfone2@gmail.com>

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,7 @@ server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://henry-kids.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://henry-kids.herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true"); //pido credenciales
   res.header(
     "Access-Control-Allow-Headers",
