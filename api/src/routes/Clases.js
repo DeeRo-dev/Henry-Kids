fff8914317da419af00d42f2fdc9ed8dfac94a20
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-12-01 22:09:39 -0500

    gio escribió getClass mal

--- api/src/routes/Clases.js
+++ api/src/routes/Clases.js
@@ -20,7 +20,7 @@ router.put("/:id", editClass);
 router.get("/titulo", async (req, res, next) => {
   try {
     let title = req.query.title; //Recibo la request en una variable
-    let classTotal = await getclass(); //Guardo mi controlador que trae todos los pokemons en una variable..
+    let classTotal = await getClass(); //Guardo mi controlador que trae todos los pokemons en una variable..
     if (title) { //Consulto si me pasan un nombre y lo busco en la variable de arriba
       let className = await classTotal.filter((el) => 
         el.title.toLowerCase().includes(title.toLowerCase())
