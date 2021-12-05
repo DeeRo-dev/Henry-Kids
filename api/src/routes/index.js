89b22d1d6061f1d87924a8053090981c7bb6d09c
felipe sanchez <felipesanchez_13@hotmail.com>
2021-12-05 00:50:41 -0300

    enviar e-mails

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const classRoutes = require("./Clases");
 const classUser = require("./user");
 const relClassUser = require("./relClassUser");
 const evaluationRoutes = require("./evaluation");
+var nodemailer = require("nodemailer");
 
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
@@ -18,6 +19,57 @@ router.use("/user", classUser);
 router.use("/", relClassUser);
 // Esto hay que moverlo a Clases.js
 router.use("/evaluation", evaluationRoutes);
+router.post("/send-emial", (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "benedict.gleason74@ethereal.email",
+      pass: "4gmRaxVam7X4cwKb2M",
+    },
+  });
+  var mailOptions = {
+    form: "remitente",
+    to: "felipesanchez231@hotmail.com",
+    subject: "enviado desde nodemailer",
+    text: "Hola mundo",
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      console.log("Email enviado");
+      res.status(200).send(mailOptions);
+    }
+  });
+});
+
+router.post("/send-emial2", (req, res) => {
+  let { userName, mail, name, url, age, sex, size, owner } = req.body;
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "felipesanchez231@gmail.com",
+      pass: "aezakmi10381",
+    },
+  });
+
+  var mailOptions = {
+    form: "remitente",
+    to: "felipesanchez_13@hotmail.com",
+    subject: "enviado desde nodemailer",
+    text: "Hola mundo",
+    
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.status(200).jsonp(req.body);
+    }
+  });
+});
 module.exports = router;
 
 // POST /category/
