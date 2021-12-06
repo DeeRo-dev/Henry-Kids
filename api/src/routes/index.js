caa5f912a25c30f11d9e1a91d0c296864f3a3462
felipe sanchez <felipesanchez_13@hotmail.com>
2021-12-06 17:48:40 -0300

    asd

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -19,57 +19,7 @@ router.use("/user", classUser);
 router.use("/", relClassUser);
 // Esto hay que moverlo a Clases.js
 router.use("/evaluation", evaluationRoutes);
-router.post("/send-emial", (req, res) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    auth: {
-      user: "benedict.gleason74@ethereal.email",
-      pass: "4gmRaxVam7X4cwKb2M",
-    },
-  });
-  var mailOptions = {
-    form: "remitente",
-    to: "felipesanchez231@hotmail.com",
-    subject: "enviado desde nodemailer",
-    text: "Hola mundo",
-  };
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      console.log("Email enviado");
-      res.status(200).send(mailOptions);
-    }
-  });
-});
 
-router.post("/send-emial2", (req, res) => {
-  let { userName, mail, name, url, age, sex, size, owner } = req.body;
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "felipesanchez231@gmail.com",
-      pass: "aezakmi10381",
-    },
-  });
-
-  var mailOptions = {
-    form: "remitente",
-    to: "felipesanchez_13@hotmail.com",
-    subject: "enviado desde nodemailer",
-    text: "Hola mundo",
-    
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else {
-      res.status(200).jsonp(req.body);
-    }
-  });
-});
 module.exports = router;
 
 // POST /category/
--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,4 +1,3 @@
-
 const { Router } = require("express");
 const {
   createUser,
@@ -9,22 +8,20 @@ const {
   getTipo,
   solTeacher,
   solAceptadaTeacher,
-  getSolicitudTecher
-
+  getSolicitudTecher,
 } = require("../controllers/user");
 // const { Router } = require("express");
 // const { createUser, getUserId, getUser } = require("../controllers/user");
 
 const router = Router();
 
-
 router.get("/", getUser);
 router.post("/", createUser);
 router.get("/:id", getUserId);
 router.delete("/:id", deleteUser);
 router.put("/:id", editUser);
-router.get("/typo/:id",getTipo)
-router.put("/solicitud/:id", solTeacher )
-router.put("/solicitud/aceptada/:id", solAceptadaTeacher )
-router.get("/solicitud/lista", getSolicitudTecher )
+router.get("/typo/:id", getTipo);
+router.put("/solicitud/:id", solTeacher);
+router.put("/solicitud/aceptada/:id", solAceptadaTeacher);
+router.get("/solicitud/lista", getSolicitudTecher);
 module.exports = router;
