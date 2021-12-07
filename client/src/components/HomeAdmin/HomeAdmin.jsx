a6eae83abf1e7693ea780d448d80fd9e6e97dee3
Derek Cabrera <cabrera.derek.adriel@gmail.com>
2021-12-07 09:47:32 -0300

    se trabaja en el home del administrador

--- client/src/components/HomeAdmin/HomeAdmin.jsx
+++ client/src/components/HomeAdmin/HomeAdmin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect }  from "react"
 import style from "./HomeAdmin.module.css"
-
-
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import { Link } from "react-router-dom";
 import  WithStyles  from "../SelectFiltros/SelectFiltros.jsx";
 
 export default function HomeAdmin(){
@@ -12,6 +12,11 @@ export default function HomeAdmin(){
     <div className={style.content}>
         <nav className={style.nav}>
           <h1 className={style.tituloNav}>Panel de Administrador: </h1> <h3>  NombreDeUsuario</h3>
+          <Link to="user/solicitud/lista">
+          <div className={style.contentNotificaciones}>
+          <NotificationsActiveIcon/><input type="button"  value="Solicitudes" className={style.botonSoli}/>
+            </div>
+          </Link>
         </nav>
         
         <div className={style.contentFiltros}>
