58aacd3bb85f64527e988fcfd30e0f4f4214b78e
Marlon De La Roche <marlondelaroch3@gmail.com>
2021-12-09 00:46:44 -0500

    update app

--- api/src/app.js
+++ api/src/app.js
@@ -1,7 +1,6 @@
 // configruacion de Expres
 const express = require("express");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
 
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
@@ -12,23 +11,11 @@ const server = express();
 
 server.name = "API";
 
-const whiteList = ["http://localhost:3000"];
-server.use(
-  cors({
-    origin: whiteList,
-    methods: "GET, POST, OPTIONS, PUT, DELETE",
-    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept",
-    optionsSuccessStatus: 200,
-    credentials: true,
-    preflightContinue: false, 
-  })
-);
-
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-/* server.use((req, res, next) => {
+server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true"); //pido credenciales
   res.header(
@@ -37,7 +24,7 @@ server.use(morgan("dev"));
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE"); //metodos que voy a aceptar en el bakend
   next();
-}); */
+});
 
 server.use("/", routes); // esto me genera /api/*
 
