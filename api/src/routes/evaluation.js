74bbbba173ec7f86d46c5b7ba71b3729b035338c
felipe sanchez <felipesanchez_13@hotmail.com>
2021-12-11 02:37:02 -0300

    funcion para validacion

--- api/src/routes/evaluation.js
+++ api/src/routes/evaluation.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { addEval, getEval } = require("../controllers/evaluation");
+const { addEval, getEval,getClasEvaUs } = require("../controllers/evaluation");
 
 const router = Router();
 
 router.post("/", addEval);
 router.get("/:idClas", getEval);
+router.get("/claseEval/:idClas",getClasEvaUs)
 
 module.exports = router;
