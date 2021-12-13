b24d66b6265d6f85257fb33ab14d7f4e0205d7a6
felipe sanchez <felipesanchez_13@hotmail.com>
2021-12-12 22:30:52 -0300

    Arreglo100%

--- client/src/components/HomeTeacher/HomeTeacher.jsx
+++ client/src/components/HomeTeacher/HomeTeacher.jsx
@@ -58,12 +58,12 @@ export default function HomeTeacher() {
               <CardTeacher
                 id={e.id}
                 title={e.title}
-                category={e.categories[0].name}
+                category={e.categories[0]?.name}
                 description={e.description}
                 video_link={e.video_link}
                 difficulty={e.difficulty}
                 game_link={e.game_link}
-                valoration={e} 
+                valoration={e.Evaluations[0]?.Promedio} 
               /> </div>)
         }
         )}
