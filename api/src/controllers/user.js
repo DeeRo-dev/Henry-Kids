72cf4222a17bb46fa3a823b8d0fe9b0a24e0ed46
Ignacio Páez <ignaciopaez96@gmail.com>
2021-12-13 17:14:44 -0300

    Update User.js

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -154,13 +154,15 @@ async function solTeacher(req, res, next) {
       },
     });
 
+    const foundUser = await User.findByPk(req.params.id);
+    const user = foundUser.toJSON();
     // Acá le ponemos mayúscula a la primer letra del nombre.
-    let newFirstName = result.firstName.charAt(0).toUpperCase() + result.firstName.slice(1)
+    let newFirstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1)
     // Acá leemos el archivo html y con el replace le decimos que cambie FIRST_NAME que se encuentra en el archivo, por el nosbre que se pasa por body firstName. (de esta forma hacemos el mail mas personal)
     let html_template = fs.readFileSync('./src/mails/templates/inProcess.html', {encoding:'utf8', flag:'r'})
     html_template = html_template.replace('FIRST_NAME', newFirstName)
     //aca le pasamos a la funcion, el email del usuario, el asunto, el template, y si es html o text.
-    sendMail(result.email, "Request in process", html_template, "html");
+    sendMail(user.email, "Request in process", html_template, "html");
 
     console.log(result);
 
