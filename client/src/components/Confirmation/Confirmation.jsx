98352ccb507bcd98f2bf32cd3f6d2913206c3fed
Ignacio Páez <ignaciopaez96@gmail.com>
2021-12-16 00:42:29 -0300

    Update Confirmation Component
    Component.jsx + Component.module.css

--- client/src/components/Confirmation/Confirmation.jsx
+++ client/src/components/Confirmation/Confirmation.jsx
@@ -1,11 +1,23 @@
-import React from "react"
-import style from "./Confirmation.module.css"
+import React from "react";
+import style from "./Confirmation.module.css";
 
-export default function Confirmation(){
+export default function Confirmation() {
   return (
-    <div className={style.confirmation}>
-      <h3>confirmation</h3>
-     <a href='http://localhost:3000/home' >AAAA</a>
- </div>
-  )
+    <div className={style.allComponent}>
+      <div className={style.confirmation}>
+        <h3 className={style.Title}>
+          Hemos enviado un <b>email</b> a tu casilla de correo para que{" "}
+          <b>verifiques la cuenta...</b>
+        </h3>
+        <img
+          className={style.imagen}
+          src="https://i.ibb.co/YcKHWBm/Recurso-9-100.jpg"
+          alt="img"
+        />
+      </div>
+      <a className={style.ir} href="http://gmail.com">
+        Ir a casilla de correos
+      </a>
+    </div>
+  );
 }
