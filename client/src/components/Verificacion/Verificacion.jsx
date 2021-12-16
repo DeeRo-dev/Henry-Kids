c3a0968436ff7445d4d0da47af79feade8b19df7
Ignacio Páez <ignaciopaez96@gmail.com>
2021-12-16 12:30:55 -0300

    componente verficacion

--- client/src/components/Verificacion/Verificacion.jsx
+++ client/src/components/Verificacion/Verificacion.jsx
@@ -1,24 +1,26 @@
-import { colors } from "@material-ui/core";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+import style from "./Verificacion.module.css";
 
 
  async function ver(id) {
- 
+
     await axios.put(`https://henry-kids.herokuapp.com/user/email/verificado/${id}`);
-  
-    // console.log(id)
   };
 
 export default function Verificacion(){
     const { id } = useParams();
     ver(id)
     return (
-      <div >
+      <div ><h3 className={style.text}>
           Su usario fue validado presione
-          <Link style={{color:"red"}} to={`/`}>  aca </Link>
-          para iniciar sesion
+          <Link style={style.ir} to={`/`}>  aca </Link>
+          para iniciar sesion</h3>
+          <img
+            className={style.imagen}
+            src="https://i.ibb.co/F55q7HC/Recurso-6-100.jpg"
+            alt="img"
+          />
       </div>
     );
   }
-  
